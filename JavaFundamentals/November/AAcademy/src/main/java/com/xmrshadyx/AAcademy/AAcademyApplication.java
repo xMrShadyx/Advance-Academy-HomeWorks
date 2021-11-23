1b980bbccaaa3f1f6f48c668ffe9d8de84f88366
Hyusein <daredevil91138@gmail.com>
2021-11-23 13:19:17 +0200

    Initial Empty Home Work

---
+++ JavaFundamentals/November/AAcademy/src/main/java/com/xmrshadyx/AAcademy/AAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.xmrshadyx.AAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AAcademyApplication.class, args);
+	}
+
+}
---
+++ JavaFundamentals/November/AAcademy/src/test/java/com/xmrshadyx/AAcademy/AAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmrshadyx.AAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
